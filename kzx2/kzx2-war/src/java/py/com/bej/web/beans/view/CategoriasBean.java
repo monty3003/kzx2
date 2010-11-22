03660002b0a9bd69b40e1731cd5e29614472d9fd
d300381 <d300381@00effaa4-834b-825f-ab5d-6280c5bfe144>
2010-11-22 11:47:38 +0000

    Beans de Categoria

---
+++ kzx2/kzx2-war/src/java/py/com/bej/web/beans/view/CategoriasBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package py.com.bej.web.beans.view;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author diego
+ */
+@ManagedBean
+@ViewScoped
+public class CategoriasBean {
+
+    /** Creates a new instance of CategoriasBean */
+    public CategoriasBean() {
+    }
+
+}
