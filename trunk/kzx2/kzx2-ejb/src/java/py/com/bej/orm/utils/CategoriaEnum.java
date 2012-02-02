19b436e7dee67a6537e8366ca3904244cf8d5c90
d300381 <d300381@00effaa4-834b-825f-ab5d-6280c5bfe144>
2012-02-02 21:51:54 +0000

    entities nuevos

---
+++ trunk/kzx2/kzx2-ejb/src/java/py/com/bej/orm/utils/CategoriaEnum.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.com.bej.orm.utils;
+
+/**
+ *
+ * @author Diego_M
+ */
+public enum CategoriaEnum {
+
+    PERSONA_DESDE(10, "Cliente Desde"),
+    PERSONA_HASTA(29, "Cliente Hasta"),
+    CLIENTE_PF(10, "Cliente Persona Fisica"),
+    CLIENTE_PJ(15, "Cliente Persona Juridica"),
+    PROVEEDOR(20, "Proveedor"),
+    COMPRA_DESDE(40, "Compra Desde"),
+    COMPRA_HASTA(45, "Compra Hasta"),
+    COMPRA_MCO(40, "Compra de Moto al Contado"),
+    COMPRA_MCR(45, "Compra de Moto a Credito"),
+    VENTA_MCO(50, "Venta de Moto al Contado"),
+    VENTA_MCR(55, "Venta de Moto a Credito"),
+    S_FRANCES(60, "Sistema Frances"),
+    S_PER(62, "Personalizado"),
+    S_ALE(65, "Sistema Aleman"),
+    S_ESP(69, "Especial"),
+    FACTURA_COMPRA_DESDE(70, "Factura de Compra desde"),
+    FACTURA_COMPRA_HASTA(75, "Factura de Compra hasta"),
+    FACTURA_VENTA_DESDE(76, "Factura de Venta desde"),
+    FACTURA_VENTA_HASTA(79, "Factura de Venta hasta"),
+    FACTURA_DESDE(70, "Factura desde"),
+    FACTURA_HASTA(70, "Factura hasta"),
+    FACTURA_COMPRA_MCO(70, "Factura de Compra de Moto al Contado"),
+    FACTURA_COMPRA_MCR(75, "Factura de Compra de Moto a Credito"),
+    FACTURA_VENTA_MCO(76, "Factura de Venta de Moto al Contado"),
+    FACTURA_VENTA_MCR(79, "Factura de Venta de Moto a Credito");
+    private int symbol;
+    private String label;
+
+    private CategoriaEnum(int symbol, String label) {
+        this.symbol = symbol;
+        this.label = label;
+    }
+
+    /**
+     * @return the symbol
+     */
+    public int getSymbol() {
+        return symbol;
+    }
+
+    /**
+     * @param symbol the symbol to set
+     */
+    public void setSymbol(int symbol) {
+        this.symbol = symbol;
+    }
+
+    /**
+     * @return the label
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * @param label the label to set
+     */
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
--- trunk/kzx2/kzx2-ejb/src/java/py/com/bej/orm/utils/Conversor.java
+++ trunk/kzx2/kzx2-ejb/src/java/py/com/bej/orm/utils/Conversor.java
@@ -6,10 +6,7 @@ package py.com.bej.orm.utils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.StringTokenizer;
 
 /**
  *
@@ -17,25 +14,11 @@ import java.util.StringTokenizer;
  */
 public class Conversor {
 
-    public static Date deStringToDate(String date) {
+    public static Date deStringToDate(String date, String pattern) {
         Date result = null;
-        Calendar cal = GregorianCalendar.getInstance();
-        String dia = null;
-        String mes = null;
-        String anho = null;
-        Integer d = null;
-        Integer m = null;
-        Integer a = null;
+        DateFormat df = new SimpleDateFormat(pattern);
         try {
-            StringTokenizer tokens = new StringTokenizer(date.toString(), "/", false);
-            dia = tokens.nextToken();
-            mes = tokens.nextToken();
-            anho = tokens.nextToken();
-            d = new Integer(dia);
-            m = new Integer(mes);
-            a = new Integer(anho);
-            cal = new GregorianCalendar(d, m, a);
-            result = cal.getTime();
+            result = df.parse(date);
         } catch (Exception e) {
             e.getLocalizedMessage();
         } finally {
@@ -43,9 +26,9 @@ public class Conversor {
         }
     }
 
-    public static String deDateToString(Date date) {
+    public static String deDateToString(Date date, String pattern) {
         String result = null;
-        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat df = new SimpleDateFormat(pattern);
         try {
             result = df.format(date);
         } catch (Exception e) {
@@ -54,7 +37,6 @@ public class Conversor {
             return result;
         }
     }
-
 
     public static byte[] deBooleanToByte(Boolean b) {
         byte[] result = {0};
